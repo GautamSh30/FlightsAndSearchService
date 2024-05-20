e53d53d4a6ca70d90a9d73a0d51e088b9683c1e6
Gautam Sharma <gautam2586272@gmail.com>
2024-05-20 12:43:08 +0530

    u

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -49,8 +49,49 @@ const getAll = async (req, res) => {
     });
   }
 };
+const get = async (req, res) => {
+  try {
+    const response = await flightService.getFlight(req.params.id);
+    return res.status(SuccessCodes.OK).json({
+      data: response,
+      success: true,
+      err: {},
+      message: "Successfully fetched the flight data",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "Not able to get the flight",
+      err: error,
+    });
+  }
+};
+
+const update = async (req, res) => {
+  try {
+    const response = await flightService.updateFlight(req.params.id, req.body);
+    return res.status(SuccessCodes.OK).json({
+      data: response,
+      success: true,
+      err: {},
+      message: "Successfully updated the flight data",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "Not able to update the flight",
+      err: error,
+    });
+  }
+};
 
 module.exports = {
   create,
   getAll,
+  get,
+  update,
 };
